365dc3bb328cbfb07b1ef19f7521f25c5b8e2ffa
carlos.santos.wd <carlos.santos.wd@gmail.com>
2021-12-01 15:09:39 -0300

    update composer to user php-webdriver instead facebook/webdriver (abandoned)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,17 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit68005abf56b0b020a08eb1aab7163151
 {
+    public static $files = array (
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '2a3c2110e8e0295330dc3d11a4cbc4cb' => __DIR__ . '/..' . '/php-webdriver/webdriver/lib/Exception/TimeoutException.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\Process\\' => 26,
         ),
         'F' => 
@@ -18,14 +26,30 @@ class ComposerStaticInit68005abf56b0b020a08eb1aab7163151
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
         'Symfony\\Component\\Process\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/process',
         ),
         'Facebook\\WebDriver\\' => 
         array (
-            0 => __DIR__ . '/..' . '/facebook/webdriver/lib',
+            0 => __DIR__ . '/..' . '/php-webdriver/webdriver/lib',
         ),
+    );
+
+    public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -33,6 +57,7 @@ class ComposerStaticInit68005abf56b0b020a08eb1aab7163151
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit68005abf56b0b020a08eb1aab7163151::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit68005abf56b0b020a08eb1aab7163151::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit68005abf56b0b020a08eb1aab7163151::$classMap;
 
         }, null, ClassLoader::class);
     }
